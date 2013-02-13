cef58fe6fdd85fe5c065f695ebf65a3eb2610d9f
Andrew Dunham <andrew@du.nham.ca>
2013-02-13 18:56:58 -0500

    Work with just globals, since I hate Pascal

--- assignment1/question3/question3.pas
+++ assignment1/question3/question3.pas
@@ -6,6 +6,7 @@ const
 type
     State = 0..20;
     StateSet = set of State;
+    StateArray = array of State;
     Token = 'a'..'z';
     Transition = record
                     source : State;
@@ -15,9 +16,9 @@ type
     TransitionList = array of Transition;
 
 var
-    initial_state : integer;
-    final_states  : array of State;
-    transitions   : array of Transition;
+    initial_state : State;
+    final_states  : StateArray;
+    transitions   : TransitionList;
 
     all_states    : StateSet;
 
@@ -56,7 +57,36 @@ begin
 end;
 
 
-function RemoveUnreachable(var start : State; var transitions : TransitionList; var all_states : StateSet) : StateSet;
+procedure PrintState();
+var
+    i : State;
+
+begin
+    writeln('------------------------------');
+    write('All states: ');
+    for i in all_states do
+    begin
+        write(i, ', ');
+    end;
+    writeln('');
+
+    writeln('Initial state: ', initial_state);
+
+    writeln('Transitions:');
+    for tmp_trans in transitions do
+        writeln('  ', tmp_trans.source, ' --> ', tmp_trans.token, ' --> ', tmp_trans.target);
+
+    writeln('Final states:');
+    for i in final_states do
+    begin
+        write(i, ', ');
+    end;
+    writeln('');
+    writeln('------------------------------');
+end;
+
+
+procedure RemoveUnreachable();
 var
     reachable_states, new_states : StateSet;
     temp, empty_set : StateSet;
@@ -64,9 +94,12 @@ var
     q : State;
     p : Transition;
 
+    new_transitions : TransitionList;
+    new_final       : StateArray;
+
 begin
-    reachable_states := [start];
-    new_states := [start];
+    reachable_states := [initial_state];
+    new_states := [initial_state];
     empty_set := [];
 
     repeat
@@ -83,12 +116,91 @@ begin
             end;
         end;
 
-        new_states := reachable_states - temp;
+        new_states := temp - reachable_states;
         reachable_states := reachable_states + new_states;
     until new_states = empty_set;
 
-    (* Return *)
-    RemoveUnreachable := reachable_states;
+    (* Create list of transitions that only include states in the reachable set *)
+    SetLength(new_transitions, 0);
+    for p in transitions do
+    begin
+        if (p.source in reachable_states) then
+        begin
+            SetLength(new_transitions, Length(new_transitions) + 1);
+            new_transitions[Length(new_transitions) - 1] := p;
+        end;
+    end;
+
+    transitions := new_transitions;
+
+    (* Remove all final states that are unreachable. *)
+    SetLength(new_final, 0);
+    for q in final_states do
+    begin
+        if (q in reachable_states) then
+        begin
+            SetLength(new_final, Length(new_final) + 1);
+            new_final[Length(new_final) - 1] := q;
+        end;
+    end;
+
+    final_states := new_final;
+
+    (* Reset set of all states to that of reachable states *)
+    all_states := reachable_states;
+end;
+
+
+procedure MinimizeDFA();
+var
+    mark_list           : array of integer;
+    i, j                : State;
+    tmp, tmp2           : State;
+    unmarked, marked    : integer;
+
+    procedure SetMark(var state1 : State; var state2 : State; var b : integer);
+    var
+        idx : integer;
+    begin
+        idx := state1 * NumberOfStates + state2;
+        mark_list[idx] := b;
+
+        idx := state2 * NumberOfStates + state1;
+        mark_list[idx] := b;
+    end;
+
+    function GetMark(var state1 : State; var state2 : State) : integer;
+    var
+        idx1, idx2 : integer;
+    begin
+        idx1 := state1 * NumberOfStates + state2;
+        idx2 := state2 * NumberOfStates + state1;
+        GetMark := mark_list[idx1] or mark_list[idx2];
+    end;
+
+begin
+    unmarked := 0;
+    marked := 1;
+
+    (* All states start as unmarked *)
+    SetLength(mark_list, NumberOfStates * NumberOfStates);
+    for i := 0 to NumberOfStates - 1 do
+    begin
+        for j := 0 to NumberOfStates - 1 do
+        begin
+            tmp := i;
+            tmp2 := j;      (* I get a strange error otherwise... *)
+            SetMark(tmp, tmp2, unmarked);
+        end;
+    end;
+
+    (* Mark pairs of final and nonfinal states *)
+    for i in final_states do
+    begin
+        for j in all_states do
+        begin
+        end;
+    end;
 end;
 
 
@@ -100,16 +212,6 @@ begin
     *)
     ReadInput();
 
-    (* Print information *)
-    writeln('The initial state is: ', initial_state);
-    for i := 0 to Length(final_states) - 1 do
-    begin
-        writeln('Final state ', i, ' is ', final_states[i]);
-    end;
-
-    for tmp_trans in transitions do
-        writeln(tmp_trans.source, ' --> ', tmp_trans.token, ' --> ', tmp_trans.target);
-
     (* Create set of states *)
     all_states := [initial_state];
     for tmp_state in final_states do
@@ -118,9 +220,13 @@ begin
     for tmp_trans in transitions do
         all_states := all_states + [tmp_trans.source, tmp_trans.target];
 
-    (* Remove unreachable states *)
-    all_states := RemoveUnreachable(initial_state, transitions, all_states);
+    PrintState();
 
-    (* Remove nondistinguishable sets *)
+    (* Remove unreachable states *)
+    writeln('Removing unreachable states...');
+    RemoveUnreachable();
+    PrintState();
+
+    (* Remove nondistinguishable states *)
 
 end.
