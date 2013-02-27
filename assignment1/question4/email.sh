1bce1c76d623e5c752c8a9359f31472c5d9c32d8
Andrew Dunham <andrew@du.nham.ca>
2013-02-27 18:39:12 -0500

    More email updates

--- assignment1/question4/email.sh
+++ assignment1/question4/email.sh
@@ -9,4 +9,4 @@
 # The seperators that are valid are anything that doesn't exist in an email - i.e.:
 #   [^A-Za-z0-9@+\._-]
 
-cd tests && grep -orhE '([^A-Za-z0-9@+\._-]|^)[A-Za-z+\._-]+@([A-Za-z0-9-]+\.)+[A-Za-z0-9-]+([^A-Za-z0-9@+\._-]|$)' .
+cd tests && grep -orhE '(?:[^A-Za-z0-9@+\._-]|^)[A-Za-z+\._-]+@([A-Za-z0-9-]+\.)+[A-Za-z0-9-]+(?:[^A-Za-z0-9@+\._-]|$)' .
