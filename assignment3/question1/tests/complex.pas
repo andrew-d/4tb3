27560850efe38127a6e529bb8e8193d878a652c0
Andrew Dunham <andrew@du.nham.ca>
2013-03-26 18:28:29 -0400

    Add more test files

---
+++ assignment3/question1/tests/complex.pas
@@ -0,0 +1,10 @@
+program complex;
+
+  var x: integer;
+
+begin
+  read(x);
+  x := (x * 1) div 1 + 0 - 0;
+  write(x); writeln;
+end.
+
---
+++ assignment3/question1/tests/swap.pas
@@ -0,0 +1,11 @@
+program swap;
+
+  var x: integer;
+
+begin
+  read(x);
+
+  x := 2 + x;
+
+  write(x); writeln;
+end.
